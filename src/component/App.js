6e9f4d2dda83658d134b2dfc7663644e774330d0
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-08 23:08:40 +0500

    move react query code to app component

--- src/component/App.js
+++ src/component/App.js
@@ -1,3 +1,22 @@
 import React from 'react';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+export default function App() {
+    function fetchUsers() {
+      return fetch(process.env.REACT_APP_URL).then(
+        res => res.json()
+      )
+    }
+  
+    const { data, status } = useQuery('users', fetchUsers);
+    if (status === 'loading') return <p>Loading...</p>
+  
+    return (
+      <div>
+        <ReactQueryDevtools/>
+        <h1>React query</h1>
+        <ul>
+          {data && data.map(user => <li key={user.id}>{user.name}</li>)}
+        </ul>
+      </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -5,25 +5,5 @@ import { useQuery } from 'react-query'
 import { ReactQueryDevtools } from 'react-query-devtools'
 
 
-export default function App() {
-  function fetchUsers() {
-    return fetch(process.env.REACT_APP_URL).then(
-      res => res.json()
-    )
-  }
-
-  const { data, status } = useQuery('users', fetchUsers);
-  if (status === 'loading') return <p>Loading...</p>
-
-  return (
-    <div>
-      <ReactQueryDevtools/>
-      <h1>React query</h1>
-      <ul>
-        {data && data.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul>
-    </div>
-  )
-}
 
 render(<App />, document.getElementById('root'));
